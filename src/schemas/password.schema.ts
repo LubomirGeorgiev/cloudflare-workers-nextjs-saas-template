1af2424aa09fa1586261ab58b0a7ab574a4e5724
Lubomir Georgiev <lubomir.georgiev.georgiev@gmail.com>
2026-07-28 20:52:11 +0700

    Set PASSWORD_MIN_LENGTH to 8

--- src/schemas/password.schema.ts
+++ src/schemas/password.schema.ts
@@ -1,6 +1,6 @@
 import { encodeValidationMessage, minMaxString } from "@/lib/validation";
 
-export const PASSWORD_MIN_LENGTH = 15;
+export const PASSWORD_MIN_LENGTH = 8;
 export const PASSWORD_MAX_LENGTH = 128;
 export const LEGACY_PASSWORD_MIN_LENGTH = 6;
 export const LEGACY_PASSWORD_MAX_LENGTH = 1024;
